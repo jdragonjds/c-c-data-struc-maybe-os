b4ec4c69a178d041c8c36e9c768da84a2fc6deb7
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-03-31 15:37:59 +0530

    Create bcd scoreboard.cpp

---
+++ bcd scoreboard.cpp
@@ -0,0 +1,134 @@
+/*     electric love #jdragon
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠋⠁⠀⠀⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡴⠃⢀⡴⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀
+⠀⠀⠀⠀⡾⠁⣠⠋⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀
+⠀⠀⠀⣸⠁⢰⠃⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠀
+⠀⠀⠀⡇⠀⡾⡀⠀⠀⠀⠀⣀⣹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀
+⠀⠀⢸⠃⢀⣇⡈⠀⠀⠀⠀⠀⠀⢀⡑⢄⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⢸⠀⢻⡟⡻⢶⡆⠀⠀⠀⠀⡼⠟⡳⢿⣦⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⣸⠀⢸⠃⡇⢀⠇⠀⠀⠀⠀⠀⡼⠀⠀⠈⣿⡗⠂⠀⠀⠀⠀⠀⠀⠀⢸⠁
+⠀⠀⡏⠀⣼⠀⢳⠊⠀⠀⠀⠀⠀⠀⠱⣀⣀⠔⣸⠁⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀
+⠀⠀⡇⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀
+⠀⢸⠃⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⢀⠀⠀⠀⠀⠀⣾⠀⠀
+⠀⣸⠀⠀⠹⡄⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠸⠀⠀⠀⠀⠀⡇⠀⠀
+⠀⡏⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀⢰⡀⠀⠀⠀⠀⠀⡇⠀⠀
+⢰⠇⡄⠀⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣧⠀⠀
+⣸⠀⡇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⢹⠀⠀⢸⠀⠀⢀⣿⠇⠀⠀⠀⠁⠀⢸⠀⠀
+⣿⠀⡇⠀⠀⠀⠀⠀⢀⡤⠤⠶⠶⠾⠤⠄⢸⠀⡀⠸⣿⣀⠀⠀⠀⠀⠀⠈⣇⠀
+⡇⠀⡇⠀⠀⡀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠸⡌⣵⡀⢳⡇⠀⠀⠀⠀⠀⠀⢹⡀
+⡇⠀⠇⠀⠀⡇⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠮⢧⣀⣻⢂⠀⠀⠀⠀⠀⠀⢧
+⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘
+⢻⠀⠈⠰⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠘⢮⣧⡀⠀⠀⠀⠀
+⠸⡆⠀⠀⠇⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠆⠀⠀⠀⠀⠀⠀⠀⠙⠳⣄⡀⢢⡀
+ oregairu */ 
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define fl i,a,b for(int i=a;i<b;i++)
+#define co cout<<
+#define ci cin>>
+#define rep(i,x,n) for(int i = x; i < n; i++) 
+#define rev(i,n,x) for(int i = n; i >= x; i--)
+#define ff            first
+#define ss            second
+#define vecll vector<long long>
+#define pii pair<long long ,long long >
+#define pip pair<int,pair<int,int>>
+#define ppp pair<pair<int,int>,pair<int,int>>
+#define inarr(arr,x,n)  for(ll i=x ;i<n;i++) cin >> arr[i];
+#define mpp make_pair
+#define pb                push_back
+#define ppb               pop_back
+#define all(x)          (x).begin(),(x).end()
+#define sz(x)           (x).size()
+#define henshin           cout<<endl;
+#define DEBUG(x) cout << (x) << '\n';
+const ll INF = int(1e9);
+void yn(int x) { if(!x)cout << "NO\n";  else if(x)cout << "YES\n";return; }
+
+const ll N=2e5+5,M=1e9+7,mod=1e9+7;
+  
+
+const int DIV = 1e5;
+
+inline int bcdtod(string s,int d) {
+     string bcd[10] ={"1110111", "0010010", "1011101", "1011011", "0111010", "1101011", "1101111", "1010010", "1111111", "1111011"};
+ int c=0;
+ rep(i,0,7){
+if(s[i]=='1'&& bcd[d][i]=='0')return -1;
+else if(s[i]=='0' && bcd[d][i]=='1')c++;
+ }
+
+ return c; 
+} 
+
+void solve() {
+ int n,m;ci n>>m; string s[n];
+int k[n][10]={{0}};int dig[n];
+vector<vector<int>> dp(n + 1, vector<int>(m + 1,-1));  
+rep(i,0,n) ci s[i];
+rep(i,0,n) 
+    rep(j,0,10){
+     k[i][j]=bcdtod(s[i],j);
+     if(k[i][j]>m)k[i][j]==-1;
+ } 
+ dp[n-1][0] = 1;
+rep(i,n-1,1) 
+    rep(j,0,m+1){
+if(dp[i][j])
+for (int d = 0; d < 10; d++) {
+          if (k[i - 1][d] != -1 && j + k[i - 1][d] <= k) {
+            dp[i - 1][j + dist[i - 1][d]] = 1;
+          }
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+for (int i = 0; i < n; i++) {
+    int now = -1;
+    for (int d = 9; d >= 0; d--) {
+      if (k[i][d] != -1 && dp[i + 1][m - k[i][d]]) {
+        now = d;
+        k -= k[i][d];
+        break;
+      }
+    }
+    assert(now != -1);
+    cout << now;
+  }
+
+
+
+ henshin}
+ 
+int main() {
+#ifdef _DEBUG
+    freopen("input.txt", "r", stdin);
+    int tt = clock();
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    cout << fixed << setprecision(15);
+    
+    int tc=1;   
+ 
+    while(tc--) { 
+ solve(); 
+      
+#ifdef _DEBUG
+        cerr << "TIME = " << clock() - tt << endl;
+        tt = clock();
+#endif
+    }
+    return 0;
+}
